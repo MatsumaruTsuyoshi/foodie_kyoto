0c3bd2f6639f3fbd89c6360b3fe698aeb49c0428
nasubibocchi <matsuhito125@gmail.com>
2022-04-05 18:29:00 +0900

    Shop domain layer

---
+++ lib/domain/repository/shop_repository.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:foodie_kyoto/data/remote/data_source/shop_data_source.dart';
+import 'package:foodie_kyoto/data/repository/result.dart';
+import 'package:foodie_kyoto/data/repository/shop_repository_impl.dart';
+import 'package:foodie_kyoto/domain/entity/shop.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final shopRepositoryProvider = Provider(
+    (ref) => ShopRepositoryImpl(dataSource: ref.read(shopDataSourceProvider)));
+
+abstract class ShopRepository {
+  Future<Result<List<Shop>>> fetchShops(
+      {required int limit, QueryDocumentSnapshot? cursor});
+}
---
+++ lib/domain/use_case/shop_use_case.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:foodie_kyoto/data/repository/result.dart';
+import 'package:foodie_kyoto/domain/entity/shop.dart';
+import 'package:foodie_kyoto/domain/repository/shop_repository.dart';
+import 'dart:async';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final shopUseCaseProvider = Provider(
+    (ref) => ShopUseCase(repository: ref.read(shopRepositoryProvider)));
+
+class ShopUseCase extends ShopRepository {
+  ShopUseCase({required ShopRepository repository}) : _repository = repository;
+
+  final ShopRepository _repository;
+
+  @override
+  Future<Result<List<Shop>>> fetchShops(
+          {required int limit, QueryDocumentSnapshot? cursor}) =>
+      _repository.fetchShops(limit: limit, cursor: cursor);
+}
