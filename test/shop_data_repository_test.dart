732c4541d50b307e840d16c38a020bef9cde34ef
nasubibocchi <matsuhito125@gmail.com>
2022-04-06 13:51:04 +0900

    Create repository test

---
+++ test/shop_data_repository_test.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:foodie_kyoto/data/model/shop_model.dart';
+import 'package:foodie_kyoto/data/remote/data_source/shop_data_source.dart';
+import 'package:foodie_kyoto/data/repository/result.dart';
+import 'package:foodie_kyoto/data/repository/shop_repository_impl.dart';
+import 'package:foodie_kyoto/domain/entity/shop.dart';
+import 'package:foodie_kyoto/domain/repository/shop_repository.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'shop_data_repository_test.mocks.dart';
+
+@GenerateMocks([ShopDataSource])
+void main() {
+  late MockShopDataSource _shopDataSource;
+
+  setUp(() {
+    _shopDataSource = MockShopDataSource();
+  });
+
+  group('fetch shops', () {
+    test('shop repository can correctly change data type shop_model to shop',
+        () async {
+      final container = ProviderContainer(overrides: [
+        shopRepositoryProvider.overrideWithProvider(
+            Provider((ref) => ShopRepositoryImpl(dataSource: _shopDataSource)))
+      ]);
+
+      final List<Map<String, dynamic>> data = [
+        {
+          'name': 'name_1',
+          'shop_id': 'shop_id_1',
+          'comment': 'comment_1',
+          'images': ['image1', 'image2'],
+          'tags': [1, 3],
+          'created_at': DateTime(2020, 1, 1),
+        },
+        {
+          'name': 'name_2',
+          'shop_id': 'shop_id_2',
+          'comment': 'comment_2',
+          'images': ['image1', 'image2'],
+          'tags': [2, 4],
+          'created_at': DateTime.now(),
+        },
+      ];
+      when(_shopDataSource.fetchShops(limit: 10)).thenAnswer((_) async {
+        return Success(data.map((e) => ShopModel.fromJson(e)).toList());
+      });
+
+      final model = container.read(shopRepositoryProvider);
+      final shopResult = await model.fetchShops(limit: 10);
+
+      // ShopModel -> Shop の変換確認
+      expect(
+        shopResult,
+        isA<Success<List<Shop>>>()
+            .having((result) => result, 'isNotNull', isNotNull),
+      );
+    });
+  });
+}
